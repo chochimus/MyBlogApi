92e6b37de48dd363510427c3095b4ab0ceb61dd8
chochimus <127354521+chochimus@users.noreply.github.com>
2024-11-23 19:32:17 -0500

    Fixed logout not deleting cookie

--- controllers/logout.js
+++ controllers/logout.js
@@ -4,6 +4,7 @@ logoutRouter.post("/", async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: true,
+    sameSite: "None",
   });
   res.status(200).json({ message: "logout successful" });
 });
