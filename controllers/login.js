6c9784bd1d7a4bb92fa418eaec5a00255778eb9e
chochimus <127354521+chochimus@users.noreply.github.com>
2024-12-09 18:05:38 -0500

    Allow more attempts to login.

--- controllers/login.js
+++ controllers/login.js
@@ -8,7 +8,7 @@ const { body, validationResult } = require("express-validator");
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 15,
   message: { error: "Too many login attempts, please try again later" },
 });
 
